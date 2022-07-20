9e7db2bbedaf986dc892828b05332186fa8af7bf
Amit <amit.ghosh@cefalo.com>
2022-07-20 13:48:16 -0500

    add sprring stater project

---
+++ src/main/java/lab/waa/three/databasedemo/DatabasedemoApplication.java
@@ -0,0 +1,13 @@
+package lab.waa.three.databasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabasedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lab/waa/three/databasedemo/DatabasedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.waa.three.databasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
