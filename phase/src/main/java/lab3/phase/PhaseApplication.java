fa8c7151b6071fc3f6586eb07057f04df0d93887
abdullah <Abdullahamzeh@hotmail.com>
2022-07-21 10:03:35 -0500

    initial

---
+++ phase/src/main/java/lab3/phase/PhaseApplication.java
@@ -0,0 +1,13 @@
+package lab3.phase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhaseApplication.class, args);
+    }
+
+}
